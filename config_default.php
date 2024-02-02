05a7806e151f6e7d099f775f69fef19502d08705
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-02-02 18:44:46 +0100

    added CRUD operations for Owners

---
+++ config_default.php
@@ -0,0 +1,12 @@
+<?php
+class Config
+{
+    public static $host = "";
+    public static $database = "";
+    public static $username = "";
+    public static $password = "";
+    public static $port = "";
+
+}
+
+?>
